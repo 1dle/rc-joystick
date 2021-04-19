92038371b63a97e261083f1d8f53427227a8d856
Bence Deli <delibnc@gmail.com>
2021-04-19 16:22:55 +0200

    SIGTERM, SIGINT handling

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,28 @@
 #include <unistd.h>
 #include "ControllerFactory.h"
 #include "RemoteRC.h"
+#include <atomic>
+#include <csignal>
+#include <cstring>
 
+std::atomic<bool> quit(false);    // signal flag
+
+void got_signal(int)
+{
+    quit.store(true);
+}
 int main()
 {
-    //g29 steering wheel
+    //these are for catching the SIGTERM signal, because if the program stopped with ctrl+c or other incorrect way,
+    //the destructors are not called.
+    //source: https://stackoverflow.com/a/4250601
+    struct sigaction sa;
+    memset( &sa, 0, sizeof(sa) );
+    sa.sa_handler = got_signal;
+    sigfillset(&sa.sa_mask);
+    sigaction(SIGINT,&sa,NULL); //sends Ctrl+C
+    sigaction(SIGTERM, &sa, NULL); //sends Clion stop button
+
     Controller* con = ControllerFactory::create(ControllerFactory::XBOX_WIRELESS);
 
     RemoteRC rc(con);
@@ -19,18 +37,19 @@ int main()
             std::cout << "joystick not found." << std::endl;
             return 1;
         }
+        JoystickEvent event;
         while (true)
         {
             // Restrict sampling rate
             usleep(800);
             // Attempt to sample an event from the joystick
-            JoystickEvent event;
             if (joystick.sample(&event))
             {
                 if(!rc.processRawInput(event)){
                     break;
                 }
             }
+            if( quit.load() ) return 0;
         }   
     }
     
